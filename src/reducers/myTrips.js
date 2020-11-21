58e7411436e46c176ab26e53dff9e02c0973374a
MMcClure11 <mcclme02@luther.edu>
2020-11-21 14:06:19 -0600

    clears state of myTrips when user logs out

--- src/reducers/myTrips.js
+++ src/reducers/myTrips.js
@@ -4,6 +4,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case "SET_MY_TRIPS":
       return action.trips
+      case "CLEAR_TRIPS":
+        return initialState
     default:
       return state
   }
